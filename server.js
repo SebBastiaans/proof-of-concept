fc9439512c440dcd5e21ca321bf6e21c77e527a1
Sbee8777 <seb.bastiaans@hva.nl>
2026-06-03 12:45:11 +0200

    Route setup #2

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+
+import { Liquid } from 'liquidjs';
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+app.set('views', './views')
+
+// MARK: Routes -------------------------------
+
+app.get('/', async (req, res) => {
+
+    const exhibitResponse = await fetch('https://fdnd-agency.directus.app/items/teylers_museum_exhibits/1?')
+    const exhibitResponseJSON = await exhibitResponse.json()
+
+    res.render('index.liquid', {
+     exhibit: exhibitResponseJSON.data
+    })
+});
+
+
+app.set('port', process.env.PORT || 8000)
+
+app.listen(app.get('port'), function () {
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
+})
+
+
+app.use((req, res, next) => {
+  res.status(404).render('404.liquid')
+})
