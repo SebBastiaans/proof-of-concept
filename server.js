ecd98bcf9f0b2e79870502ea3f3d0a3cb126204f
Sbee8777 <seb.bastiaans@hva.nl>
2026-06-04 12:04:34 +0200

    HTML prototype

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', async (req, res) => {
     const exhibitResponse = await fetch('https://fdnd-agency.directus.app/items/teylers_museum_exhibits/1?')
     const exhibitResponseJSON = await exhibitResponse.json()
 
-    const sectionResponse = await fetch('https://fdnd-agency.directus.app/items/teylers_museum_exhibits_sections')
+    const sectionResponse = await fetch('https://fdnd-agency.directus.app/items/teylers_museum_exhibits_sections?fields=*')
     const sectionResponseJSON = await sectionResponse.json()
 
     const personResponse = await fetch('https://fdnd-agency.directus.app/items/teylers_museum_persons')
